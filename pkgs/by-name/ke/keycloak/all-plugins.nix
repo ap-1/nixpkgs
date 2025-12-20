a8580fe1e7e39e6cc09069fac89504c020d59d41
Anish Pallati <i@anish.land>
2025-12-20 11:36:46 -0500

    keycloak: reuse existing junixsocket packages

--- pkgs/by-name/ke/keycloak/all-plugins.nix
+++ pkgs/by-name/ke/keycloak/all-plugins.nix
@@ -1,4 +1,9 @@
-{ callPackage, fetchMavenArtifact }:
+{
+  callPackage,
+  fetchMavenArtifact,
+  junixsocket-common,
+  junixsocket-native-common,
+}:
 
 {
   scim-for-keycloak = callPackage ./scim-for-keycloak { };
@@ -11,21 +16,8 @@
 
   # junixsocket provides Unix domain socket support for JDBC connections,
   # which is required for connecting to PostgreSQL via Unix socket.
-  junixsocket-common =
-    (fetchMavenArtifact {
-      groupId = "com.kohlschutter.junixsocket";
-      artifactId = "junixsocket-common";
-      version = "2.10.1";
-      hash = "sha256-GeX3YVrSKT81Mrw/mRsxOWwRYYNOidmmqgx975OcZyk=";
-    }).passthru.jar;
-
-  junixsocket-native-common =
-    (fetchMavenArtifact {
-      groupId = "com.kohlschutter.junixsocket";
-      artifactId = "junixsocket-native-common";
-      version = "2.10.1";
-      hash = "sha256-ASbOC68c61de9ReAfU0rFLnzLwYYAgThLsc6tKdyVno=";
-    }).passthru.jar;
+  junixsocket-common = junixsocket-common.passthru.jar;
+  junixsocket-native-common = junixsocket-native-common.passthru.jar;
 
   # These could theoretically be used by something other than Keycloak, but
   # there are no other quarkus apps in nixpkgs (as of 2023-08-21)
