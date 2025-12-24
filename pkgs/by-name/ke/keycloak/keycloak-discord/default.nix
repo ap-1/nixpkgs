5f2f460692e4ea10f1873838bd35d01d6c2315d2
Anish Pallati <i@anish.land>
2025-12-24 15:17:17 -0500

    keycloak.plugins.keycloak-discord: add aarch64-linux mvnHash

--- pkgs/by-name/ke/keycloak/keycloak-discord/default.nix
+++ pkgs/by-name/ke/keycloak/keycloak-discord/default.nix
@@ -16,10 +16,27 @@ maven.buildMavenPackage rec {
   };
 
   mvnHash =
-    if stdenv.hostPlatform.isDarwin then
-      "sha256-Or7VOZwz4NfDtb0kmHbbTYE/avAc+H8+Y6JPw+HGjxs="
-    else
-      "sha256-uhm++MGgTN32/xbHNd+Z3Hes9Q5tl8ztIQ92LxMWKjg=";
+    let
+      platform = stdenv.hostPlatform;
+      key =
+        if platform.isDarwin && platform.isAarch64 then
+          "aarch64-darwin"
+        else if platform.isDarwin && platform.isx86_64 then
+          "x86_64-darwin"
+        else if platform.isLinux && platform.isAarch64 then
+          "aarch64-linux"
+        else if platform.isLinux && platform.isx86_64 then
+          "x86_64-linux"
+        else
+          throw "unsupported platform: ${platform.system}";
+    in
+    {
+      aarch64-darwin = "sha256-Or7VOZwz4NfDtb0kmHbbTYE/avAc+H8+Y6JPw+HGjxs=";
+      x86_64-darwin = lib.fakeHash;
+      aarch64-linux = "sha256-I5qjhfAXPXMb+1SPG29t/IKH/zBQqdnu3U7dYSQhTL8=";
+      x86_64-linux = "sha256-uhm++MGgTN32/xbHNd+Z3Hes9Q5tl8ztIQ92LxMWKjg=";
+    }
+    .${key};
 
   installPhase = ''
     runHook preInstall
